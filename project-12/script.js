3e01f6b87c7473ade6b259b03d4d14665139cb80
stefankrajisnik <stefankrajisnik95@gmail.com>
2022-02-22 23:30:35 +0100

    Update of freq. asked question

--- project-12/script.js
+++ project-12/script.js
@@ -1,7 +1,33 @@
-const toggles = document.querySelectorAll('.faq-toggle')
+// const toggles = document.querySelectorAll('.faq-toggle')
 
- toggles.forEach(toggle => {
-     toggle.addEventListener('click', () => {
-         toggle.parentNode.classList.toggle('active')
-     })
- })
+//  toggles.forEach(toggle => {
+//      toggle.addEventListener('click', () => {
+//          toggle.parentNode.classList.toggle('active')
+//      })
+//  })
+
+const faqContainer = document.querySelector('.faq-container')
+
+const childOfFaqContainer = faqContainer.querySelectorAll('.faq')
+
+console.log(childOfFaqContainer)
+
+const closeOpenFaq = () => {
+    const openFaq = document.querySelector('.faq.active');
+    console.log(openFaq);
+    openFaq.classList.toggle('active');
+}
+
+childOfFaqContainer.forEach(child => {
+    child.addEventListener('click',(e) =>{
+        // TODO: if clicked on already active one, do nothing
+        if (child.classList.contains('active')) {
+            console.log('already active');
+            return true;
+        }
+
+        // TODO: close open ones
+        closeOpenFaq();
+        child.classList.toggle('active')
+    })
+});
