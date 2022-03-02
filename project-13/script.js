7e06d017317959d5b3ef4f05fbb089a797cd3f12
stefankrajisnik <stefankrajisnik95@gmail.com>
2022-03-02 15:17:55 +0100

    Project random picker is updated and finished

--- project-13/script.js
+++ project-13/script.js
@@ -3,11 +3,19 @@ const textarea = document.getElementById('textarea')
 
 textarea.focus()
 
-textarea.addEventListener('keyup',(e)=>createTags(e.target.value))
+textarea.addEventListener('keyup',(e)=>{
+    createTags(e.target.value)
+
+    if(e.key === "Enter"){
+        setTimeout(()=>{
+            e.target.value=''
+        },10)
+        randomSelect()
+    }
+})
 
 function createTags(input){
     const tags = input.split(',').filter(tag => tag.trim()!=="").map(tag=>tag.trim())
-    // console.log(tags)
 
     tagsEl.innerHTML= ''
 
@@ -17,7 +25,44 @@ tags.forEach(tag => {
     tagEl.innerText = tag
     tagsEl.appendChild(tagEl)
     
-})
+}) 
+}
+
+function randomSelect(){
+    const times = 30
+
+    const interval = setInterval( () => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(()=>{
+            unHighlightTag(randomTag)
+        },100)
+    },100);
+
+    setTimeout(()=> {
+        clearInterval(interval)
+
+        setTimeout(()=>{
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        },100)
+    },times * 100)
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag){
+    tag.classList.remove('highlight')
 }
 
 
