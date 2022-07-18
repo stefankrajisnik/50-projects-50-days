0865928d9c494ba1df6dc646cc401cbe4a37f3c0
stefankrajisnik <stefankrajisnik95@gmail.com>
2022-07-18 16:04:51 +0200

    search function added

--- project-42/script.js
+++ project-42/script.js
@@ -1,27 +1,39 @@
-const filter = document.querySelector('#filter')
-const result = document.querySelector('#result')
-let listItems = []
+const filter = document.querySelector('#filter');
+const result = document.querySelector('#result');
+let listItems = [];
 
-getData()
+getData();
+
+let filterUsers = (searchTerm) => {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
+            item.classList.remove('hide');
+        }else{
+            item.classList.add('hide');
+        }
+    } )
+};
+
+filter.addEventListener('input', (e) => filterUsers(e.target.value))
 
 async function getData() { 
-    const res = await fetch('https://randomuser.me/api/?results=50')
-    const { results } = await res.json()
+    const res = await fetch('https://randomuser.me/api/?results=50');
+    const { results } = await res.json();
 
     //clearing result
-    console.log(results)
-    result.innerHTML = ''
+    result.innerHTML = '';
 
     results.forEach(user => { 
-       let userElement = document.createElement('li')
+       let userElement = document.createElement('li');
+       listItems.push(userElement);
+
        userElement.innerHTML = `
        <img src="${user.picture.large}">
        <h4>${user.name.first} ${user.name.last}</h4>
         <p>Name is: ${user.location.city}, ${user.location.country}</p>
-       `
-       result.appendChild(userElement)
+    `;
+       
+       result.appendChild(userElement);
     })
-    
-}
-
+};
 
