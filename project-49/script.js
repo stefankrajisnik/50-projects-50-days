e528bee80b39b130e0ef627d354e648885e300dd
stefankrajisnik <stefankrajisnik95@gmail.com>
2022-08-15 20:52:56 +0200

    add project 49

--- project-49/script.js
+++ project-49/script.js
@@ -1,51 +1,97 @@
 const input = document.querySelector('.input');
-const todosList = document.querySelector('.todos');
-let tasksArr = [];
+const todosUl = document.querySelector('.todos');
+
+
+window.onload = loadFromLS();
+window.onload = input.focus();
+window.onload = updateLS();
 
 input.addEventListener('keypress', (e) => {
+    const todosArr = [];
     if (e.key === 'Enter') {
-        const todos = JSON.parse(localStorage.getItem('task'))
-        console.log(todos)
-
         let inputTask = input.value;
 
         const liEl = document.createElement('li');
         liEl.classList.add('.singleTaskEl')
         liEl.textContent = inputTask;
-        todosList.appendChild(liEl);
+        todosUl.appendChild(liEl);
 
         //prevent reloading page
-        e.preventDefault()
+        // e.preventDefault()
         input.value = ''
 
         //add entered task in array
         const allTaskEl = document.querySelectorAll('li')
         allTaskEl.forEach(task => {
-            tasksArr.push({
+            todosArr.push({
                 text: task.innerText,
                 completed: task.classList.contains('completed')
-            }) 
+            })
         });
-        // tasksArr.push(inputTask);
 
-        //store every single task from array into the Local Storage
-        localStorage.setItem('task',JSON.stringify(tasksArr))
+        //store every single task object from array into the Local Storage
+        localStorage.setItem('task', JSON.stringify(todosArr))
 
 
-        
+
     } else {
         console.log('key is not enter')
     };
-})
+});
+
+function loadFromLS() {
+    const todos = JSON.parse(localStorage.getItem('task'))
+    if (todos) {
+
+        console.log(todos)
+
+        for (let i = 0; i < todos.length; i++) {
+            const liEl = document.createElement('li');
+            liEl.textContent = todos[i].text
+            todosUl.appendChild(liEl)
+
+            if (todos[i].completed) {
+                liEl.classList.add('completed')
+                console.log('yeaaa ' + liEl.classList)
+            }
+        }
+    } else {
+        return
+    }
+
+};
+
+const liS = document.querySelectorAll('li');
+
+liS.forEach((li) => {
+    li.addEventListener('click', () => {
+        li.classList.toggle('completed')
+        updateLS()
+    })
+});
+
+function updateLS() {
+    const todosArr = [];
+    const todosEl = document.querySelectorAll('li')
+
+    todosEl.forEach((todoEl) =>{
+        todosArr.push({
+            text:todoEl.textContent,
+            completed:todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('task', JSON.stringify(todosArr))
+};
 
 
     //DOES NOT WORK WITH GETELEMENTBYCLASSNAME 
       //add entered task in array
     //   const allTaskEl = document.getElementsByClassName('.singleTaskEl')
     //   allTaskEl.forEach(task => {
-    //       tasksArr.push({
+    //       todosArr.push({
     //           text: task.innerText,
     //           completed: task.classList.contains('completed')
-    //       }) 
+    //       })
     //   });
-      // tasksArr.push(inputTask);
+      // todosArr.push(inputTask);
